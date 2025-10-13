5993822c343502835dedecf8e39e4a23ca4721c9
Earl Syx Hernandez <hernandezearlsyxmail@gmail.com>
2025-10-13 21:08:25 +0800

    Constructor

---
+++ Constructor/Node.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+public class Node
+{
+    public int Value { get; set; }
+    public Node Next { get; set; }
+
+    public Node(int value)
+    {
+        Value = value;
+        Next = null;
+    }
+}
+
+public class LinkedList
+{
+    public Node Head { get; set; }
+    public Node Tail { get; set; }
+    public int Length { get; set; }
+
+    public LinkedList(int value)
+    {
+        Node newNode = new Node(value);
+        Head = newNode;
+        Tail = Head;
+        Length = 1;
+    }
+
+    public void PrintList()
+    {
+        Node temp = Head;
+        while (temp != null)
+        {
+            Console.WriteLine(temp.Value);
+            temp = temp.Next;
+        }
+    }
+
+    public void GetHead()
+    {
+        if (Head == null)
+        {
+            Console.WriteLine("Head: null");
+        }
+        else
+        {
+            Console.WriteLine($"Head: {Head.Value}");
+        }
+    }
+
+    public void GetTail()
+    {
+        if (Tail == null)
+        {
+            Console.WriteLine("Tail: null");
+        }
+        else
+        {
+            Console.WriteLine($"Tail: {Tail.Value}");
+        }
+    }
+
+    public void GetLength()
+    {
+        Console.WriteLine($"Length: {Length}");
+    }
+}
+
+
---
+++ Constructor/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+
+LinkedList myLinkedList = new LinkedList(5);
+
+myLinkedList.GetHead();
+myLinkedList.GetTail();
+myLinkedList.GetLength();
+
+Console.WriteLine("\nLinked List:");
+myLinkedList.PrintList();
